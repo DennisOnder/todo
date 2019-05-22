f524a5dbd3efc29989dfab3ae4c0dbfa393564ee
Dennis Onder <webdev.dennisonder@gmail.com>
2019-05-22 17:07:17 +0200

    Main file

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/adem/todo"
+)
+
+func main() {
+	todo.AddTodo("Buy milk")
+	todo.AddTodo("Fill out important documents")
+
+	printTodos()
+	// ☐  An older todo task created in an earlier session (id: 4)
+	// ☐  Buy milk (id: 5)
+	// ☐  Fill out important documents (id: 6)
+
+	if todoItem := todo.FindByTitle("Buy milk"); todoItem != nil {
+		todoItem.Done = true
+	}
+
+	printTodos()
+	// ☐  An older todo task created in an earlier session (id: 4)
+	// ☑  Buy milk (id: 5)
+	// ☐  Fill out important documents (id: 6)
+}
+
+func printTodos() {
+	for _, todo := range todo.Todos {
+		fmt.Println(todo)
+	}
+}
