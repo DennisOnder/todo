910f5150fd06d74e94b434a957253b6cfbcf4902
Dennis Onder <webdev.dennisonder@gmail.com>
2019-05-22 17:14:51 +0200

    Folder structuring

--- main.go
+++ main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"fmt"
 
-	"github.com/DennisOnder/todo"
+	"github.com/DennisOnder/todo/packages/todos"
 )
 
 func main() {
-	todo.AddTodo("Buy milk")
-	todo.AddTodo("Fill out important documents")
+	todos.AddTodo("Buy milk")
+	todos.AddTodo("Fill out important documents")
 
 	printTodos()
 	// ☐  An older todo task created in an earlier session (id: 4)
 	// ☐  Buy milk (id: 5)
 	// ☐  Fill out important documents (id: 6)
 
-	if todoItem := todo.FindByTitle("Buy milk"); todoItem != nil {
+	if todoItem := todos.FindByTitle("Buy milk"); todoItem != nil {
 		todoItem.Done = true
 	}
 
@@ -26,7 +26,7 @@ func main() {
 }
 
 func printTodos() {
-	for _, todo := range todo.Todos {
+	for _, todo := range todos.Todos {
 		fmt.Println(todo)
 	}
 }
